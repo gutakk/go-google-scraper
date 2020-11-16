58b3e698c28d329ec6fe9bfeb487daadfdf83da3
Gut <thiramate@nimblehq.co>
2020-11-16 15:24:01 +0700

    Add index html

--- config/router.go
+++ config/router.go
@@ -1,15 +1,23 @@
 package config
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gutakk/go-google-scraper/controllers"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Home",
+		})
+	})
 
 	health := new(controllers.HealthController)
-
 	router.GET("/health", health.Status)
 
 	return router
