6207ea0fe21bdabfba9a806da91344f8bf1b10a4
Gut <thiramate@nimblehq.co>
2020-11-25 12:20:55 +0700

    Pass email to home page and handle to show greeting or link

--- controllers/home.go
+++ controllers/home.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	session "github.com/gutakk/go-google-scraper/helpers/session"
+	"github.com/gutakk/go-google-scraper/models"
 )
 
 type HomeController struct{}
@@ -13,7 +15,15 @@ func (h *HomeController) applyRoutes(engine *gin.Engine) {
 }
 
 func (h *HomeController) displayHome(c *gin.Context) {
+	userID := session.Get(c, "user_id")
+
+	var user models.User
+	if userID != nil {
+		user, _ = models.FindOneUserByID(userID)
+	}
+
 	c.HTML(http.StatusOK, "home", gin.H{
 		"title": "Home",
+		"email": user.Email,
 	})
 }
