328f47fd80183392e60b98b378dfbb534e48e491
Gut <thiramate@nimblehq.co>
2020-12-03 16:52:36 +0700

    Add unit test for Save in keyword service

--- services/keyword_service/keyword_test.go
+++ services/keyword_service/keyword_test.go
@@ -3,9 +3,70 @@ package keyword_service
 import (
 	"testing"
 
+	"github.com/bxcodec/faker/v3"
+	"github.com/gutakk/go-google-scraper/db"
+	"github.com/gutakk/go-google-scraper/models"
+	testDB "github.com/gutakk/go-google-scraper/tests/db"
 	"github.com/stretchr/testify/suite"
+	"golang.org/x/crypto/bcrypt"
 	"gopkg.in/go-playground/assert.v1"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+type KeywordServiceDbTestSuite struct {
+	suite.Suite
+	keywordService Keyword
+}
+
+func (s *KeywordServiceDbTestSuite) SetupTest() {
+	database, _ := gorm.Open(postgres.Open(testDB.ConstructTestDsn()), &gorm.Config{})
+	db.GetDB = func() *gorm.DB {
+		return database
+	}
+
+	_ = db.GetDB().AutoMigrate(&models.User{}, &models.Keyword{})
+
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(faker.Password()), bcrypt.DefaultCost)
+	user := models.User{Email: faker.Email(), Password: string(hashedPassword)}
+	db.GetDB().Create(&user)
+
+	s.keywordService = Keyword{CurrentUserID: user.ID}
+}
+
+func (s *KeywordServiceDbTestSuite) TearDownTest() {
+	db.GetDB().Exec("DELETE FROM keywords")
+	db.GetDB().Exec("DELETE FROM users")
+}
+
+func TestKeywordServiceDbTestSuite(t *testing.T) {
+	suite.Run(t, new(KeywordServiceDbTestSuite))
+}
+
+func (s *KeywordServiceDbTestSuite) TestSaveWithValidParams() {
+	record := []string{"Hazard", "Ronaldo", "Neymar", "Messi", "Mbappe"}
+	result, err := s.keywordService.Save(record)
+
+	assert.Equal(s.T(), 5, len(result))
+	assert.Equal(s.T(), nil, err)
+}
+
+func (s *KeywordServiceDbTestSuite) TestSaveWithValidInvalidUser() {
+	record := []string{"Hazard", "Ronaldo", "Neymar", "Messi", "Mbappe"}
+	keywordService := Keyword{}
+	result, err := keywordService.Save(record)
+
+	assert.Equal(s.T(), "Something went wrong, please try again", err.Error())
+	assert.Equal(s.T(), nil, result)
+}
+
+func (s *KeywordServiceDbTestSuite) TestSaveWithEmptyRecord() {
+	record := []string{}
+	result, err := s.keywordService.Save(record)
+
+	assert.Equal(s.T(), "Invalid data", err.Error())
+	assert.Equal(s.T(), nil, result)
+}
 
 type KeywordServiceTestSuite struct {
 	suite.Suite
