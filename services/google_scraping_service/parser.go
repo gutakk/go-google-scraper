df2f35cfa69879f79123f45aaff506098c457c5b
Gut <thiramate@nimblehq.co>
2020-12-08 15:51:43 +0700

    Add parse links function to DRY

--- services/google_scraping_service/parser.go
+++ services/google_scraping_service/parser.go
@@ -21,53 +21,39 @@ func GoogleResultParser(resp *http.Response) (string, error) {
 	return "hello", nil
 }
 
-func countTopPosAdwords(doc *goquery.Document) int {
-	return doc.Find("#tads > div").Length()
-}
-
-func fetchTopPosAdwordLinks(doc *goquery.Document) []string {
-	var topPosAdwordLinks []string
-
-	doc.Find("#tads > div .Krnil").Each(func(i int, s *goquery.Selection) {
-		href, exists := s.Attr("href")
-		if exists {
-			topPosAdwordLinks = append(topPosAdwordLinks, href)
-		}
-	})
-
-	return topPosAdwordLinks
-}
-
-func countTotalAdwords(doc *goquery.Document, topPosAdwords int) int {
-	return doc.Find("#tadsb > div").Length() + topPosAdwords
+func countLinks(doc *goquery.Document) int {
+	return len(parseLinks(doc, "a"))
 }
 
 func countNonAdwords(doc *goquery.Document) int {
 	return doc.Find("#rso > div[class=g]").Length()
 }
 
-func fetchNonAdwordLinks(doc *goquery.Document) []string {
-	var nonAdwordLinks []string
-
-	doc.Find("#rso > div[class=g] .yuRUbf > a").Each(func(i int, s *goquery.Selection) {
-		href, exists := s.Attr("href")
-		if exists {
-			nonAdwordLinks = append(nonAdwordLinks, href)
-		}
-	})
-
-	return nonAdwordLinks
+func countTopPosAdwords(doc *goquery.Document) int {
+	return doc.Find("#tads > div").Length()
 }
 
-func countLinks(doc *goquery.Document) int {
+func countTotalAdwords(doc *goquery.Document, topPosAdwords int) int {
+	return doc.Find("#tadsb > div").Length() + topPosAdwords
+}
+
+func fetchNonAdwordLinks(doc *goquery.Document) []string {
+	return parseLinks(doc, "#rso > div[class=g] .yuRUbf > a")
+}
+
+func fetchTopPosAdwordLinks(doc *goquery.Document) []string {
+	return parseLinks(doc, "#tads > div .Krnil")
+}
+
+func parseLinks(doc *goquery.Document, selector string) []string {
 	var links []string
 
-	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+	doc.Find(selector).Each(func(i int, s *goquery.Selection) {
 		href, exists := s.Attr("href")
 		if exists {
 			links = append(links, href)
 		}
 	})
 
-	return len(links)
+	return links
 }
