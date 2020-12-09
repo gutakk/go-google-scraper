543c36deb803b7d8024c6cd43fe795d26ed3c039
Gut <thiramate@nimblehq.co>
2020-12-09 16:18:23 +0700

    Add controller class name to body

--- helpers/html/render_html.go
+++ helpers/html/render_html.go
@@ -9,6 +9,7 @@ import (
 func RenderWithError(c *gin.Context, status int, view string, title string, err error, data map[string]interface{}) {
 	ginview.HTML(c, status, view, gin.H{
 		"title": title,
+		"view":  view,
 		"error": err.Error(),
 		"data":  data,
 	})
@@ -17,6 +18,7 @@ func RenderWithError(c *gin.Context, status int, view string, title string, err 
 func RenderWithFlash(c *gin.Context, status int, view string, title string, data map[string]interface{}) {
 	ginview.HTML(c, status, view, gin.H{
 		"title":         title,
+		"view":          view,
 		"noticeFlashes": session.Flashes(c, "notice"),
 		"errorFlashes":  session.Flashes(c, "error"),
 		"data":          data,
