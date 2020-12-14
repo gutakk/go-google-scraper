a80ab86000b4b9eb201f89ce7e6f8dcdaeaea33b
Gut <thiramate@nimblehq.co>
2020-12-14 15:18:16 +0700

    Add more VCR test

--- services/google_scraping_service/request_test.go
+++ services/google_scraping_service/request_test.go
@@ -9,14 +9,44 @@ import (
 	"gopkg.in/go-playground/assert.v1"
 )
 
-func TestRequest(t *testing.T) {
-	r, _ := recorder.New("../../tests/fixture/vcr")
+func TestRequestWithValidKeyword(t *testing.T) {
+	r, _ := recorder.New("../../tests/fixture/vcr/valid_keyword")
 
 	googleRequest := GoogleRequest{Keyword: "AWS", Transport: r}
 	resp, _ := googleRequest.Request()
 
 	p, err := ioutil.ReadAll(resp.Body)
 	isGoogleSearchPage := err == nil && strings.Index(string(p), "<title>AWS") > 0
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, true, isGoogleSearchPage)
+
+	_ = r.Stop()
+}
+
+func TestRequestWithBlankSpaceKeyword(t *testing.T) {
+	r, _ := recorder.New("../../tests/fixture/vcr/blank_space_keyword")
+
+	googleRequest := GoogleRequest{Keyword: "A W S", Transport: r}
+	resp, _ := googleRequest.Request()
+
+	p, err := ioutil.ReadAll(resp.Body)
+	isGoogleSearchPage := err == nil && strings.Index(string(p), "<title>A W S") > 0
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, true, isGoogleSearchPage)
+
+	_ = r.Stop()
+}
+
+func TestRequestWithThaiKeyword(t *testing.T) {
+	r, _ := recorder.New("../../tests/fixture/vcr/thai_keyword")
+
+	googleRequest := GoogleRequest{Keyword: "สวัสดี", Transport: r}
+	resp, _ := googleRequest.Request()
+
+	p, err := ioutil.ReadAll(resp.Body)
+	isGoogleSearchPage := err == nil && strings.Index(string(p), "<title>สวัสดี") > 0
 
 	assert.Equal(t, nil, err)
 	assert.Equal(t, true, isGoogleSearchPage)
