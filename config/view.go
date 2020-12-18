5e5f678688b75fe6530fece7e8971e18563e4878
Gut <thiramate@nimblehq.co>
2020-12-18 11:29:49 +0700

    Use scrape result and scrape result list partial

--- config/view.go
+++ config/view.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"html/template"
+
 	"github.com/foolin/goview"
 )
 
@@ -10,6 +12,18 @@ func AppGoviewConfig() goview.Config {
 		Extension:    ".html",
 		Master:       "layouts/application",
 		DisableCache: false,
+		Partials: []string{
+			"partials/scrape_result",
+			"partials/scrape_result_list",
+		},
+		Funcs: template.FuncMap{
+			"scrapeResult": func(title string, value interface{}) map[string]interface{} {
+				return map[string]interface{}{
+					"title": title,
+					"value": value,
+				}
+			},
+		},
 	}
 }
 
