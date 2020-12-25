97617185b0f46811f7b988b7a46a0e3e6e12605c
Gut <thiramate@nimblehq.co>
2020-12-25 18:16:36 +0700

    Update struct name

--- controllers/api/token.go
+++ controllers/api/token.go
@@ -5,13 +5,13 @@ import (
 	"github.com/gutakk/go-google-scraper/config"
 )
 
-type TokenController struct{}
+type TokenAPIController struct{}
 
-func (t *TokenController) ApplyRoutes(engine *gin.RouterGroup) {
+func (t *TokenAPIController) ApplyRoutes(engine *gin.RouterGroup) {
 	engine.POST("/token", t.generateToken)
 }
 
-func (t *TokenController) generateToken(c *gin.Context) {
+func (t *TokenAPIController) generateToken(c *gin.Context) {
 	err := config.GetOAuthServer().HandleTokenRequest(c.Writer, c.Request)
 	if err != nil {
 		c.JSON(400, nil)
