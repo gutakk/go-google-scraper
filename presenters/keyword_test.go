2c12610fc014ab88655b359a6588a9e4096ab390
Gut <thiramate@nimblehq.co>
2020-12-25 14:51:49 +0700

    Add keyword presenter test

--- presenters/keyword_test.go
+++ presenters/keyword_test.go
@@ -1,10 +1,13 @@
 package presenters
 
 import (
+	"encoding/json"
 	"testing"
 	"time"
 
 	"github.com/gutakk/go-google-scraper/models"
+
+	"github.com/bxcodec/faker/v3"
 	"gopkg.in/go-playground/assert.v1"
 	"gorm.io/gorm"
 )
@@ -20,4 +23,34 @@ func TestFormattedCreatedAtWithValidTime(t *testing.T) {
 	result := keywordPresenter.FormattedCreatedAt()
 
 	assert.Equal(t, "December 16, 2020", result)
+}
+
+func TestKeywordResultWithValidKeywordModels(t *testing.T) {
+	nonAdwordLinks, _ := json.Marshal([]string{"non-ad-link1", "non-ad-link2"})
+	topPositionAdwordLinks, _ := json.Marshal([]string{"top-ad-link1", "top-ad-link2"})
+
+	keyword := models.Keyword{
+		Keyword:                 faker.Name(),
+		Status:                  models.Pending,
+		LinksCount:              10,
+		NonAdwordLinks:          nonAdwordLinks,
+		TopPositionAdwordsCount: 10,
+		TopPositionAdwordLinks:  topPositionAdwordLinks,
+		TotalAdwordsCount:       10,
+		HtmlCode:                "test-html",
+	}
+
+	keywordPresenter := KeywordPresenter{Keyword: keyword}
+	result := keywordPresenter.KeywordResult()
+
+	assert.Equal(t, "0", result.ID)
+	assert.Equal(t, keyword.Keyword, result.Keyword)
+	assert.Equal(t, models.Pending, result.Status)
+	assert.Equal(t, 10, result.LinksCount)
+	assert.Equal(t, 10, result.TopPositionAdwordsCount)
+	assert.Equal(t, []string{"non-ad-link1", "non-ad-link2"}, result.NonAdwordLinks)
+	assert.Equal(t, 10, result.TotalAdwordsCount)
+	assert.Equal(t, []string{"top-ad-link1", "top-ad-link2"}, result.TopPositionAdwordLinks)
+	assert.Equal(t, "test-html", result.HtmlCode)
+	assert.Equal(t, "", result.FailedReason)
 }
