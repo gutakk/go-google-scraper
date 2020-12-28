48c0e10f5b5053d55c11e37e9501a84a78b185ed
Gut <thiramate@nimblehq.co>
2020-12-28 18:32:47 +0700

    Update log

--- oauth/oauth_server.go
+++ oauth/oauth_server.go
@@ -24,7 +24,7 @@ func SetupOAuthServer() {
 
 	// use PostgreSQL token store with pgx.Connection adapter
 	adapter := pgx4adapter.NewConn(pgxConn)
-	log.Printf("=============== %v", adapter)
+	log.Printf("=============== %v", pgxConn)
 	tokenStore, _ := pg.NewTokenStore(adapter, pg.WithTokenStoreGCInterval(time.Minute))
 	defer tokenStore.Close()
 
