baca32e8d30401087cd6728b62910a0680197fd8
Gut <thiramate@nimblehq.co>
2021-01-04 14:17:33 +0700

    Add password authorization handler

--- controllers/api_v1/token.go
+++ controllers/api_v1/token.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gutakk/go-google-scraper/oauth"
+	"github.com/gutakk/go-google-scraper/services/login_api_service"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,6 +17,9 @@ func (t *TokenAPIController) ApplyRoutes(engine *gin.RouterGroup) {
 }
 
 func (t *TokenAPIController) generateToken(c *gin.Context) {
+	server := oauth.GetOAuthServer()
+	server.SetPasswordAuthorizationHandler(login_api_service.PasswordAuthorizationHandler)
+
 	err := oauth.GetOAuthServer().HandleTokenRequest(c.Writer, c.Request)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, nil)
