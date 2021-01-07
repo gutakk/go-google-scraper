4d4fc6a7ab8655a4ef89fb1c186813bcde02864b
Gut <thiramate@nimblehq.co>
2021-01-07 14:58:57 +0700

    Update validate token middleware

--- middlewares/validate_token.go
+++ middlewares/validate_token.go
@@ -3,15 +3,27 @@ package middlewares
 import (
 	"net/http"
 
+	"github.com/gutakk/go-google-scraper/helpers/api_helper"
 	"github.com/gutakk/go-google-scraper/oauth"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-oauth2/oauth2/v4/errors"
 )
 
 func ValidateToken(c *gin.Context) {
 	_, err := oauth.GetOAuthServer().ValidationBearerToken(c.Request)
 	if err != nil {
-		http.Error(c.Writer, err.Error(), http.StatusUnauthorized)
+		errorResponse := &api_helper.ErrorResponseObject{
+			Status: http.StatusUnauthorized,
+		}
+
+		if err.Error() == "sql: no rows in result set" {
+			errorResponse.Detail = errors.ErrInvalidAccessToken.Error()
+		} else {
+			errorResponse.Detail = err.Error()
+		}
+
+		c.JSON(http.StatusUnauthorized, errorResponse.ConstructErrorResponse())
 		c.Abort()
 	}
 }
