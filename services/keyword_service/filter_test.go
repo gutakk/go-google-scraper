219bb280530c50684b6ea0da2de6d65500d0d1d3
Gut <thiramate@nimblehq.co>
2021-01-14 12:35:54 +0700

    Add test for filter

---
+++ services/keyword_service/filter_test.go
@@ -0,0 +1,44 @@
+package keyword_service_test
+
+import (
+	"testing"
+
+	"github.com/gutakk/go-google-scraper/services/keyword_service"
+
+	"gopkg.in/go-playground/assert.v1"
+)
+
+func TestGetConditionFromQueryWithValidQueryStringKeywordTitle(t *testing.T) {
+	queryString := map[string][]string{
+		"keyword-title": {"test"},
+	}
+	result := keyword_service.GetConditionFromQuery(queryString)
+	expectedResult := []string{"LOWER(keyword) LIKE LOWER('%test%')"}
+
+	assert.Equal(t, expectedResult, result)
+}
+
+func TestGetConditionFromQueryWithInvalidQueryStringKey(t *testing.T) {
+	queryString := map[string][]string{
+		"invalid-key": {"test"},
+	}
+	result := keyword_service.GetConditionFromQuery(queryString)
+	expectedResult := []string{}
+
+	assert.Equal(t, expectedResult, result)
+}
+
+func TestGetConditionFromQueryWithEmptyQueryString(t *testing.T) {
+	queryString := map[string][]string{}
+	result := keyword_service.GetConditionFromQuery(queryString)
+	expectedResult := []string{}
+
+	assert.Equal(t, expectedResult, result)
+}
+
+func TestGetConditionFromQueryWithNilQueryString(t *testing.T) {
+	result := keyword_service.GetConditionFromQuery(nil)
+	expectedResult := []string{}
+
+	assert.Equal(t, expectedResult, result)
+}
