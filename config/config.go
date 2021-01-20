68b7ba49937ff74b34530ecea2b70e83c8efe0f4
Gut <thiramate@nimblehq.co>
2021-01-20 13:54:05 +0700

    Refactor config/config.go

--- config/config.go
+++ config/config.go
@@ -1,19 +1,29 @@
 package config
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
+	var err error
 	switch gin.Mode() {
 	case gin.ReleaseMode:
-		_ = godotenv.Load(".env." + gin.ReleaseMode)
+		err = godotenv.Load(".env." + gin.ReleaseMode)
 	case gin.TestMode:
-		_ = godotenv.Load(".env." + gin.TestMode)
+		err = godotenv.Load(".env." + gin.TestMode)
 	default:
-		_ = godotenv.Load(".env." + gin.DebugMode)
+		err = godotenv.Load(".env." + gin.DebugMode)
 	}
 
-	_ = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Load %s env error: %s", gin.Mode(), err)
+	}
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Load env error: %s", err)
+	}
 }
