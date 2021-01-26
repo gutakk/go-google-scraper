a737f313efd0325953dd44a7e29210e948a05e3f
Gut <thiramate@nimblehq.co>
2021-01-26 13:19:02 +0700

    Add checkbox input type

--- helpers/view/filter_keyword_input.go
+++ helpers/view/filter_keyword_input.go
@@ -1,6 +1,6 @@
 package view
 
-func FilterKeywordPartialInput(filter map[string][]string, label string, name string, placeholder string) map[string]interface{} {
+func FilterKeywordPartialInput(filter map[string][]string, label string, inputType string, className string, name string, placeholder string) map[string]interface{} {
 	var filterValue string
 	if len(filter) > 0 {
 		filterValue = filter[name][0]
@@ -9,6 +9,8 @@ func FilterKeywordPartialInput(filter map[string][]string, label string, name st
 	return map[string]interface{}{
 		"filter":            filterValue,
 		"filterLabel":       label,
+		"inputType":         inputType,
+		"className":         className,
 		"filterName":        name,
 		"filterPlaceholder": placeholder,
 	}
