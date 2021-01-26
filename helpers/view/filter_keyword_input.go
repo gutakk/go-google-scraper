ae89ccdf67bd2c7dfa2856827e5f057771b26278
Gut <thiramate@nimblehq.co>
2021-01-26 13:31:15 +0700

    Add test for filter by adwords existence

--- helpers/view/filter_keyword_input.go
+++ helpers/view/filter_keyword_input.go
@@ -1,9 +1,14 @@
 package view
 
 func FilterKeywordPartialInput(filter map[string][]string, label string, inputType string, className string, name string, placeholder string) map[string]interface{} {
-	var filterValue string
+	var filterValue interface{}
 	if len(filter) > 0 {
-		filterValue = filter[name][0]
+		switch inputType {
+		case "text":
+			filterValue = filter[name][0]
+		case "checkbox":
+			filterValue = filter[name]
+		}
 	}
 
 	return map[string]interface{}{
