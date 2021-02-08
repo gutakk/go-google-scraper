fd0077c795d31c5235fffdc29e0786b552d9e7bb
Gut <thiramate@nimblehq.co>
2021-02-08 17:04:58 +0700

    Add error constant

---
+++ helpers/error_handler/error_constant.go
@@ -0,0 +1,13 @@
+package helpers
+
+const (
+	ChangeToRootDirFailure   = "Failed to change to root directory: "
+	ConnectToDatabaseFailure = "Failed to connect database: "
+	DeleteRedisJobFailure    = "Failed to delete redis job: "
+	HashPasswordFailure      = "Failed to hash password: "
+	JSONMarshalFailure       = "Failed to marshal json: "
+	JSONUnmarshalFailure     = "Failed to unmarshal json: "
+	MigrateDatabaseFailure   = "Failed to migrate database: "
+	ReadResponseBodyFailure  = "Failed to read response body: "
+	StartOAuthServerFailute  = "Failed to setup oauth server: "
+)
